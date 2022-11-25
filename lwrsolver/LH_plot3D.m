86a483cb3d1ec695712ea1827db328977c042316
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2022-11-25 17:39:51 +0800

    Modify LWR Solver plot formatting

--- lwrsolver/LH_plot3D.m
+++ lwrsolver/LH_plot3D.m
@@ -1,13 +1,6 @@
-function LH_plot3D(tScale, xScale, N,k, fd)
-
-    surf(tScale, xScale, N',ones(size(k')),'EdgeColor', 'none')
-    view([-110 30]);
-    light('Position',[-15 300 14],'Style','local');
-    zlabel({'vehicle count'});
-
-    % % Create xlabel
-    xlabel({'time (s)'});
-    % 
-    % % Create ylabel
-    ylabel({'x (m)'});
+function LH_plot3D(tScale,xScale,N,k,fd)
+    surf(tScale, xScale, N',ones(size(k')),'EdgeColor','none')
+    set(gca,'TickLabelInterpreter','latex')
+    view([-110 30]), light('Position',[-15 300 14],'Style','local')
+    xlabel('$$t$$','Interpreter','latex'), ylabel('$$x$$','Interpreter','latex'), zlabel('$$N$$','Interpreter','latex');
 end
