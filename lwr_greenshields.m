4a1a91b13710595ba4c27ecbaa9dd124f95b2051
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2022-11-29 08:30:59 +0800

    Adjust variable names + Add variables repr. initial and boundary space/time limits

--- lwr_greenshields.m
+++ lwr_greenshields.m
@@ -2,7 +2,7 @@ addpath('lwrsolver'), mkdir ../Figures
 clear
 
 % Define state variables.
-vfree = 30; rhomax = 0.2;
+vfree = 30; kmax = 0.2;
 % Define space variables.
 xmin = 0; xmax = 1000;
 tmin = 0; tmax = 50;
@@ -14,41 +14,44 @@ xscale = xmin:dx:xmax;
 tscale = tmin:dt:tmax;
 x = ones(size(tscale')) * xscale;
 t = tscale' * ones(size(xscale));
+% Define initial and boundary values.
+xbound = [xmin xmax]; kinit = 100;
+tusbound = [tmin tmax]; kus = 1;
+tdsbound = [tmin tmax]; kds = 0;
 
 % Create a Greenshields fundamental diagram.
-fd = LH_Greenshields(vfree,rhomax);
+fd = LH_Greenshields(vfree,kmax);
 
 % Create a spatial domain problem environment.
 pbEnv = LH_general(fd,xmin,xmax);
 % Define initial vehicle density and up/down-stream traffic flows.
-pbEnv.setIniDens([xmin xmax],90e-3);
-pbEnv.setUsFlows([tmin tmax],1);
-pbEnv.setDsFlows([tmin tmax],0);
+pbEnv.setIniDens(xbound,kinit*1e-03);
+pbEnv.setUsFlows(tusbound,kus);
+pbEnv.setDsFlows(tdsbound,kds);
 
 % Calculate the global and active solution components.
 result = pbEnv.explSol(t,x);
 N = result{1}; % Extract Moskowitz function values.
 component = result{2}; % Extract active components.
-% Compute active component densities, vehicular state-space flows, and
-% space mean speed.
-K = pbEnv.density(t,x,component);
-Q = fd.flow(K);
-U = Q./K;
+% Compute active component densities, vehicle state-space flows, vehicle
+% state-space speeds, and the shock-wave propagation speed.
+k = pbEnv.density(t,x,component);
+q = fd.flow(k);
+v = q./k;
+U = gradient(N);
 
-LH_plot3D(tscale,xscale,N,K,fd)
+LH_plot3D(tscale,xscale,N,k,fd)
 fig = gcf; fig.PaperPositionMode = 'auto'; fig_pos = fig.PaperPosition; fig.PaperSize = [fig_pos(3) fig_pos(4)];
-print('../Figures/lwr_greenshield_plot3d.pdf','-dpdf');
+print(['../Figures/lwr_greenshields_' num2str(kinit) '_plot3d.pdf'],'-dpdf');
 figure
-LH_plot2D(tscale,xscale,N,K,fd)
+LH_plot2D(tscale,xscale,N,k,fd)
 fig = gcf; fig.PaperPositionMode = 'auto'; fig_pos = fig.PaperPosition; fig.PaperSize = [fig_pos(3) fig_pos(4)];
-print('../Figures/lwr_greenshield_plot2d.pdf','-dpdf');
+print(['../Figures/lwr_greenshields_' num2str(kinit) '_plot2d.pdf'],'-dpdf');
 figure
-flow2d(tscale,xscale,Q,N,fd)
+flow2d(tscale,xscale,q,N,fd)
 fig = gcf; fig.PaperPositionMode = 'auto'; fig_pos = fig.PaperPosition; fig.PaperSize = [fig_pos(3) fig_pos(4)];
-print('../Figures/lwr_greenshield_flow2d.pdf','-dpdf');
+print(['../Figures/lwr_greenshields_' num2str(kinit) '_flow2d.pdf'],'-dpdf');
 figure
-smsp2d(tscale,xscale,U,N,fd)
+smsp2d(tscale,xscale,v,N,fd)
 fig = gcf; fig.PaperPositionMode = 'auto'; fig_pos = fig.PaperPosition; fig.PaperSize = [fig_pos(3) fig_pos(4)];
-print('../Figures/lwr_greenshield_smsp2d.pdf','-dpdf');
-
-NX = gradient(N);
+print(['../Figures/lwr_greenshields_' num2str(kinit) '_smsp2d.pdf'],'-dpdf');
