07b37b0c283e212bb90928285233518cd7fec2b0
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2022-11-29 08:29:16 +0800

    Adjust colour map to include red colour ranges

--- lwrsolver/LH_plot2D.m
+++ lwrsolver/LH_plot2D.m
@@ -1,8 +1,9 @@
 function LH_plot2D(tScale,xScale,N,k,fd)
+    colormap(jet(256))
     hold on
     set(gca,'TickLabelInterpreter','latex')
     contourf(tScale,xScale,k',64,'LineColor','none'), contour(tScale,xScale,N',30,'k'), caxis([0 fd.kappa])
-    colorbar('YTick',[0 fd.density(0) fd.kappa],'YTickLabel',{'$$\rho_{0}$$','$$\rho_{\mathrm{crit}}$$','$$\rho_{\mathrm{max}}$$'},'TickLabelInterpreter','latex')
+    colorbar('YTick',[0 fd.density(0) fd.kappa],'YTickLabel',{'$$k_{0}$$','$$k_{\mathrm{crit}}$$','$$k_{\mathrm{max}}$$'},'TickLabelInterpreter','latex')
     xlabel('$$t$$','Interpreter','latex'), ylabel('$$x$$','Interpreter','latex')
     hold off
 end
