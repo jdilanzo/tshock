7462840821fa0e2ecfc34392e07685e0eb8ed06c
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2022-11-29 08:29:42 +0800

    Adjust variable names + colour map to include red colour ranges

--- flow2d.m
+++ flow2d.m
@@ -1,9 +1,10 @@
-function flow2d(tscale,xscale,Q,N,fd)
+function flow2d(tscale,xscale,q,N,fd)
+    colormap(jet(256))
     hold on
     set(gca,'TickLabelInterpreter','latex')
-    contourf(tscale,xscale,Q',64,'LineColor','none'), contour(tscale,xscale,N',30,'k')
+    contourf(tscale,xscale,q',64,'LineColor','none'), contour(tscale,xscale,N',30,'k')
     h = colorbar; lims = get(h,'Limits');
-    colorbar('YTick',[lims(1) lims(2)],'YTickLabel',{'$$0$$','$$Q_{\mathrm{max}}$$'},'TickLabelInterpreter','latex')
+    colorbar('YTick',[lims(1) lims(2)],'YTickLabel',{'$$0$$','$$q_{\mathrm{max}}$$'},'TickLabelInterpreter','latex')
     xlabel('$$t$$','Interpreter','latex'), ylabel('$$x$$','Interpreter','latex')
     hold off
 end
--- smsp2d.m
+++ smsp2d.m
@@ -1,9 +1,10 @@
-function smsp2d(tscale,xscale,U,N,fd)
+function smsp2d(tscale,xscale,v,N,fd)
+    colormap(jet(256))
     hold on
     set(gca,'TickLabelInterpreter','latex')
-    contourf(tscale,xscale,U',64,'LineColor','none'), contour(tscale,xscale,N',30,'k')
+    contourf(tscale,xscale,v',64,'LineColor','none'), contour(tscale,xscale,N',30,'k')
     h = colorbar; lims = get(h,'Limits');
-    colorbar('YTick',[lims(1) lims(2)],'YTickLabel',{'$$0$$','$$U_{\mathrm{max}}$$'},'TickLabelInterpreter','latex')
+    colorbar('YTick',[lims(1) lims(2)],'YTickLabel',{'$$0$$','$$v_{\mathrm{max}}$$'},'TickLabelInterpreter','latex')
     xlabel('$$t$$','Interpreter','latex'), ylabel('$$x$$','Interpreter','latex')
     hold off
 end
